7f4b773615946ab6e1e54d4198d08e29a2b9c7bc
Arafat Al Mahmud <arafat.almahmud89@gmail.com>
2017-10-06 17:50:59 +0600

    publish

--- assets/javascripts/global.js
+++ assets/javascripts/global.js
@@ -1,1 +1,1 @@
-jQuery(document).ready(function(t){t(".post > .row.content a").addClass("ga-event"),t(".hello > .row.content a").addClass("ga-event"),t("a.ga-event").attr("onclick","ga('send','event','LinkTo',this.href,'http://localhost:4000');")});
+jQuery(document).ready(function(a){a(".post > .row.content a").addClass("ga-event"),a(".hello > .row.content a").addClass("ga-event"),a("a.ga-event").attr("onclick","ga('send','event','LinkTo',this.href,'https://arafat-al-mahmud.github.io/');")});
